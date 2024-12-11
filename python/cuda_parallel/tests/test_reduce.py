6c6a8bcedbd414c83379fcbfc1d94d78e8550f1e
Ashwin Srinath <shwina@users.noreply.github.com>
2024-12-11 11:45:47 -0500

    Format with Ruff

--- python/cuda_parallel/tests/test_reduce.py
+++ python/cuda_parallel/tests/test_reduce.py
@@ -167,9 +167,7 @@ def test_device_sum_cache_modified_input_it(
     dtype_inp = numpy.dtype(supported_value_type)
     dtype_out = dtype_inp
     input_devarr = numba.cuda.to_device(numpy.array(l_varr, dtype=dtype_inp))
-    i_input = iterators.CacheModifiedInputIterator(
-        input_devarr, modifier="stream"
-    )
+    i_input = iterators.CacheModifiedInputIterator(input_devarr, modifier="stream")
     _test_device_sum_with_iterator(
         l_varr, start_sum_with, i_input, dtype_inp, dtype_out, use_numpy_array
     )
@@ -218,8 +216,7 @@ def test_device_sum_map_mul2_count_it(
     dtype_inp = numpy.dtype(vtn_inp)
     dtype_out = numpy.dtype(vtn_out)
     i_input = iterators.TransformIterator(
-        mul2,
-        iterators.CountingIterator(dtype_inp.type(start_sum_with))
+        mul2, iterators.CountingIterator(dtype_inp.type(start_sum_with))
     )
     _test_device_sum_with_iterator(
         l_varr, start_sum_with, i_input, dtype_inp, dtype_out, use_numpy_array
@@ -255,7 +252,7 @@ def test_device_sum_map_mul_map_mul_count_it(
         iterators.TransformIterator(
             mul_funcs[fac_mid],
             iterators.CountingIterator(dtype_inp.type(start_sum_with)),
-        )
+        ),
     )
     _test_device_sum_with_iterator(
         l_varr, start_sum_with, i_input, dtype_inp, dtype_out, use_numpy_array
